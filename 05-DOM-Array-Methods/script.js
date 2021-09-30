ad31522ce7a52339c0d28a0e1784c0e251388e7c
crackerFactory64 <lee.mander@gmail.com>
2021-09-30 13:56:19 +0100

    set up and ui

---
+++ 05-DOM-Array-Methods/script.js
@@ -0,0 +1,8 @@
+const addUserButton = document.querySelector("#add");
+const doubleMoneyButton = document.querySelector("#double");
+const filterButton = document.querySelector("#filter");
+const sortButton = document.querySelector("#sort");
+const calcButton = document.querySelector("#calc");
+
+const outputWindow = document.querySelector(".window__output");
+const total = document.querySelector(".app__total");

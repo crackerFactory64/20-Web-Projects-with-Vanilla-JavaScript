e3b04069b5f7fb890ee0088a7ba79b1381371c5e
crackerFactory64 <lee.mander@gmail.com>
2021-09-30 15:16:10 +0100

    add user functionality

--- 05-DOM-Array-Methods/script.js
+++ 05-DOM-Array-Methods/script.js
@@ -1,8 +1,64 @@
-const addUserButton = document.querySelector("#add");
-const doubleMoneyButton = document.querySelector("#double");
-const filterButton = document.querySelector("#filter");
-const sortButton = document.querySelector("#sort");
-const calcButton = document.querySelector("#calc");
+const addUserButton = document.getElementById("add");
+const doubleMoneyButton = document.getElementById("double");
+const filterButton = document.getElementById("filter");
+const sortButton = document.getElementById("sort");
+const calcButton = document.getElementById("calc");
 
 const outputWindow = document.querySelector(".window__output");
 const total = document.querySelector(".app__total");
+
+addUserButton.addEventListener("click", () => {
+  getRandomUser();
+});
+
+let data = [];
+
+getRandomUser();
+getRandomUser();
+getRandomUser();
+getRandomUser();
+
+async function getRandomUser() {
+  const res = await fetch("https://randomuser.me/api");
+  const data = await res.json();
+  const user = data.results[0];
+  const newUser = {
+    name: `${user.name.first} ${user.name.last}`,
+    wealth: Math.floor(Math.random() * 1000000),
+  };
+  addData(newUser);
+}
+
+function addData(obj) {
+  console.log(obj);
+  data.push(obj);
+  updateDOM();
+}
+
+function updateDOM() {
+  output = "";
+  data.forEach((obj) => {
+    output = `<p class="window__person"><span class="window__name">${
+      obj.name
+    }</span><span class="window__wealth">${formatWealth(
+      obj.wealth
+    )}</span></p>`;
+  });
+  outputWindow.innerHTML += output;
+}
+
+function formatWealth(figure) {
+  let result = "";
+  let figureArray = figure.toString().split("");
+
+  if (figureArray.length == 5) {
+    figureArray.splice(2, 0, ",");
+  } else if (figureArray.length == 6) {
+    figureArray.splice(3, 0, ",");
+  } else if (figureArray.length == 7) {
+    figureArray.splice(1, 0, ",");
+    figureArray.splice(5, 0, ",");
+  }
+
+  return (result = `£${figureArray.join("")}`);
+}

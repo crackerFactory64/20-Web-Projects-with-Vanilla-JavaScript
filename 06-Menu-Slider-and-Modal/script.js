c21b84e25bfa8677c77f1fc3f80033f9eae268cb
crackerFactory64 <lee.mander@gmail.com>
2021-10-05 12:33:22 +0100

    menu slide animation

--- 06-Menu-Slider-and-Modal/script.js
+++ 06-Menu-Slider-and-Modal/script.js
@@ -3,7 +3,12 @@ const modal = document.getElementById("modal");
 const closeModal = document.getElementById("close-modal");
 const openCloseMenu = document.getElementById("menu-button");
 const menu = document.getElementById("menu");
+const container = document.querySelectorAll(".container");
 
 openCloseMenu.addEventListener("click", () => {
   menu.classList.toggle("menu--open");
+  container.forEach((element) => {
+    element.classList.toggle("container--sidebar");
+  });
+  openCloseMenu.classList.toggle("menu-button--sidebar");
 });

7ab2d9dbcfced9c74631d00f4ea927ce1cb05700
crackerFactory64 <lee.mander@gmail.com>
2021-10-05 13:26:31 +0100

    modal functionality

--- 06-Menu-Slider-and-Modal/script.js
+++ 06-Menu-Slider-and-Modal/script.js
@@ -1,5 +1,6 @@
 const openModal = document.getElementById("sign-up");
 const modal = document.getElementById("modal");
+const modalBG = document.querySelector(".modal-bg");
 const closeModal = document.getElementById("close-modal");
 const openCloseMenu = document.getElementById("menu-button");
 const menu = document.getElementById("menu");
@@ -12,3 +13,16 @@ openCloseMenu.addEventListener("click", () => {
   });
   openCloseMenu.classList.toggle("menu-button--sidebar");
 });
+
+openModal.addEventListener("click", () => {
+  openCloseModal();
+});
+
+closeModal.addEventListener("click", () => {
+  openCloseModal();
+});
+
+function openCloseModal() {
+  modal.classList.toggle("modal--active");
+  modalBG.classList.toggle("modal-bg--active");
+}

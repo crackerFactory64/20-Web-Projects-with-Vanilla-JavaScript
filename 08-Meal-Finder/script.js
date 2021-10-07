76cd5caebbf1b3bd3c77b82b5bdf234e815aace4
crackerFactory64 <lee.mander@gmail.com>
2021-10-07 16:50:10 +0100

    select meal functionality

--- 08-Meal-Finder/script.js
+++ 08-Meal-Finder/script.js
@@ -6,13 +6,11 @@ const resultsContainer = document.querySelector(".results");
 const searchTermEl = document.getElementById("search-term");
 const resultsEl = document.getElementById("results");
 
-const recipeTitleEl = document.getElementById("recipe-title");
-const recipeImgEl = document.getElementById("recipe-img");
-const recipeInsctructionsEl = document.getElementById("recipe-instructions");
-const recipeIngredientsEl = document.getElementById("recipe-ingredients");
+const recipe = document.getElementById("recipe");
 
 let searchTerm = "";
 let results = [];
+let resultElArr = []; //to be populated with the generated result html elements
 
 searchButton.addEventListener("click", (e) => {
   e.preventDefault();
@@ -46,13 +44,25 @@ async function getResults(search) {
   } else {
     searchError.innerHTML = "No results found.";
   }
+  resultsArr = document.querySelectorAll(".result"); //populates resultsArr
+
+  //adds event listener to each result element that triggers selectMeal on click
+  resultsArr.forEach((result) => {
+    result.addEventListener("click", () => {
+      for (let i = 0; i < results.length; i++) {
+        if (result.id === results[i].idMeal) {
+          selectMeal(results[i]);
+        }
+      }
+    });
+  });
 }
 
 function updateDOM() {
   let resultsOutput = "";
   results.forEach((meal) => {
     resultsOutput += `
-        <div class="result">
+        <div class="result" id="${meal.idMeal}">
               <div class="result__overlay">
                 <h2 class="result__name" id="result-name">${meal.strMeal}</h2>
               </div>
@@ -67,4 +77,32 @@ function updateDOM() {
   });
   resultsContainer.style.display = "block";
   resultsEl.innerHTML = resultsOutput;
+}
+
+function selectMeal(meal) {
+  recipe.innerHTML = `
+        <h2 class="recipe__title">${meal.strMeal}</h2>
+          <img
+            src="${meal.strMealThumb}"
+            alt="${meal.strMeal}"
+            class="recipe__img"
+           
+          />
+          <div class="recipe__info">
+            <p class="recipe__category">${meal.strCategory}</p>
+            <br />
+            <p class="recipe__cuisine">${meal.strArea}</p>
+          </div>
+          <p class="recipe__instructions">
+          ${meal.strInstructions}
+          </p>
+          <h2 class="recipe__title">Ingredients</h2>
+          <ul class="recipe__ingredients">
+            <li class="recipe__ingredient">${meal.strIngredient1}</li>
+            <li class="recipe__ingredient">${meal.strIngredient2}</li>
+            <li class="recipe__ingredient">${meal.strIngredient3}</li>
+            <li class="recipe__ingredient">${meal.strIngredient4}</li>
+          </ul>
+    `;
+  recipe.style.display = "block";
 }

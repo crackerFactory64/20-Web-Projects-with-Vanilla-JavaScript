994c78365930bc7781b04b620d612f7259738c19
crackerFactory64 <lee.mander@gmail.com>
2021-10-11 11:26:53 +0100

    Update script.js

--- 08-Meal-Finder/script.js
+++ 08-Meal-Finder/script.js
@@ -130,55 +130,13 @@ function selectMeal(meal) {
 }
 
 function getIngredients(meal) {
-  let ingredientsArr = [
-    meal.strIngredient1,
-    meal.strIngredient2,
-    meal.strIngredient3,
-    meal.strIngredient4,
-    meal.strIngredient5,
-    meal.strIngredient7,
-    meal.strIngredient8,
-    meal.strIngredient9,
-    meal.strIngredient10,
-    meal.strIngredient11,
-    meal.strIngredient12,
-    meal.strIngredient13,
-    meal.strIngredient14,
-    meal.strIngredient15,
-    meal.strIngredient16,
-    meal.strIngredient17,
-    meal.strIngredient18,
-    meal.strIngredient19,
-    meal.strIngredient20,
-  ];
-
-  let measureArr = [
-    meal.strMeasure1,
-    meal.strMeasure2,
-    meal.strMeasure3,
-    meal.strMeasure4,
-    meal.strMeasure5,
-    meal.strMeasure7,
-    meal.strMeasure8,
-    meal.strMeasure9,
-    meal.strMeasure10,
-    meal.strMeasure11,
-    meal.strMeasure12,
-    meal.strMeasure13,
-    meal.strMeasure14,
-    meal.strMeasure15,
-    meal.strMeasure16,
-    meal.strMeasure17,
-    meal.strMeasure18,
-    meal.strMeasure19,
-    meal.strMeasure20,
-  ];
-
   ingredients = "";
 
-  for (let i = 0; i < ingredientsArr.length; i++) {
-    if (ingredientsArr[i]) {
-      ingredients += `<li class="recipe__ingredient">${ingredientsArr[i]} - ${measureArr[i]}</li>`;
+  for (let i = 1; i < 20; i++) {
+    if (meal[`strIngredient${i}`]) {
+      ingredients += `<li class="recipe__ingredient">${
+        meal[`strIngredient${i}`]
+      } - ${meal[`strMeasure${i}`]} </li>`;
     }
   }
 

69fc9fa6a887eee337db14b660d83edb0dd0e5c0
crackerFactory64 <lee.mander@gmail.com>
2021-10-19 16:22:30 +0100

    refactoring start

--- 09-Expense-Tracker/script.js
+++ 09-Expense-Tracker/script.js
@@ -8,9 +8,7 @@ const amountInput = document.getElementById("amount");
 const error = document.getElementById("error");
 const addButton = document.getElementById("button");
 
-let balance = 0;
-let income = 0;
-let expense = 0;
+let transactions = [];
 
 function validateForm() {
   if (!nameInput.value || !amountInput.value) {
@@ -23,9 +21,18 @@ function validateForm() {
 
 function addNewTransaction() {
   let name = nameInput.value.trim();
-  let amount = formatMoney(amountInput.value.trim());
-  let eventHTML = "";
-  if (amountInput.value < 0) {
+  let amount = amountInput.value.trim();
+  let transaction = {
+    ID: Math.floor(Math.random() * 1000000),
+    name: name,
+    amount: parseFloat(amount),
+  };
+
+  transactions.push(transaction);
+  console.log(transactions);
+
+  populateHistory();
+  /*if (amountInput.value < 0) {
     eventHTML += `
         <div class="history__event">
             <button class="history__delete">X</button>
@@ -44,23 +51,46 @@ function addNewTransaction() {
   }
 
   history.innerHTML += eventHTML;
+  calculateTotals();*/
+}
+
+function populateHistory() {
+  let output = "";
+
+  transactions.forEach((transaction) => {
+    if (transaction.amount < 0) {
+      output += `
+        <div class="history__event">
+          <p class="history__delete">X</p>
+          <p>${transaction.name}</p>
+          <p>${formatMoney(transaction.amount)}</p>
+        </div>
+      `;
+    } else {
+      output += `
+        <div class="history__event history__event--pos">
+          <p class="history__delete">X</p>
+          <p>${transaction.name}</p>
+          <p>+${formatMoney(transaction.amount)}</p>
+        </div>
+      `;
+    }
+  });
+
+  history.innerHTML = output;
   calculateTotals();
 }
 
 function calculateTotals() {
-  let input = parseFloat(amountInput.value.trim());
+  let amounts = transactions.map((transaction) => transaction.amount);
+  console.log(amounts);
+  let balance = amounts.reduce((total, amount) => {
+    return (total += amount);
+  });
 
-  balance = balance + input;
-
-  if (input > 0) {
-    income = income + input;
-  } else {
-    expense = expense + input;
-  }
-
-  updateDOM();
+  balanceEl.innerHTML = formatMoney(balance);
 }
-
+/*
 function updateDOM() {
   if (balance >= 0) {
     balanceEl.classList.add("balance__sum--pos");
@@ -74,7 +104,7 @@ function updateDOM() {
 
   nameInput.value = "";
   amountInput.value = "";
-}
+}*/
 
 function formatMoney(figure) {
   const moneyFormatter = new Intl.NumberFormat("en-GB", {

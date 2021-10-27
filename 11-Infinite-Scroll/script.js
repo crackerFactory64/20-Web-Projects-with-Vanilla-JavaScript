e1546257a054be3ad9e79a7bf98a0419872fa6ce
crackerFactory64 <lee.mander@gmail.com>
2021-10-27 16:40:28 +0100

    quick refactor

--- 11-Infinite-Scroll/script.js
+++ 11-Infinite-Scroll/script.js
@@ -5,32 +5,43 @@ const loader = document.getElementById("loader");
 let page = 1;
 
 getPosts();
+showPosts();
 
-function getPosts() {
-  fetch(`https://jsonplaceholder.typicode.com/posts?_limit=5&_page=${page}`)
-    .then((response) => response.json())
-    .then((json) => {
-      let output = "";
-      json.forEach((post) => {
-        output += `<div class="post">
+async function getPosts() {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=5&_page=${page}`
+  );
+  const data = await response.json();
+  return data;
+}
+
+async function showPosts() {
+  const posts = await getPosts();
+
+  let output = "";
+  posts.forEach((post) => {
+    output += `<div class="post">
           <p class="post__id">${post.id}</p>
           <h2 class="post__title">${post.title}</h2>
           <p class="post__body">
             ${post.body}
           </p>
         </div>`;
-      });
-      postsEl.innerHTML += output;
-      page++;
-    });
+  });
+  postsEl.innerHTML += output;
+  page++;
 }
 
 function updatePosts() {
-  if (document.body.scrollHeight == window.scrollY + window.innerHeight) {
+  if (
+    document.body.scrollHeight == window.scrollY + window.innerHeight &&
+    page <= 20
+  ) {
     loader.classList.add("main__loader--show");
     setTimeout(() => {
       loader.classList.remove("main__loader--show");
       getPosts();
+      showPosts();
     }, 1000);
   }
 }

371c4d8abd8b1e44b9b1a6372972f2c38696822f
crackerFactory64 <lee.mander@gmail.com>
2021-10-27 15:46:30 +0100

    getPosts

--- 11-Infinite-Scroll/script.js
+++ 11-Infinite-Scroll/script.js
@@ -1,2 +1,35 @@
 const searchBar = document.getElementById("search-bar");
 const postsEl = document.getElementById("posts");
+
+let page = 1;
+
+getPosts();
+
+function getPosts() {
+  fetch(`https://jsonplaceholder.typicode.com/posts?_limit=5&_page=${page}`)
+    .then((response) => response.json())
+    .then((json) => {
+      let output = "";
+      json.forEach((post) => {
+        output += `<div class="post">
+          <p class="post__id">${post.id}</p>
+          <h2 class="post__title">${post.title}</h2>
+          <p class="post__body">
+            ${post.body}
+          </p>
+        </div>`;
+      });
+      postsEl.innerHTML += output;
+      page++;
+    });
+}
+
+function updatePosts() {
+  if (document.body.scrollHeight == window.scrollY + window.innerHeight) {
+    getPosts();
+  }
+}
+
+window.addEventListener("scroll", () => {
+  updatePosts();
+});

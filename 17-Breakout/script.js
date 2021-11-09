a531d53cca1be091bb14b7facbc76726104604fa
crackerFactory64 <lee.mander@gmail.com>
2021-11-09 15:29:37 +0000

    done a ball

--- 17-Breakout/script.js
+++ 17-Breakout/script.js
@@ -1,6 +1,15 @@
 const rulesPanel = document.getElementById("rules-panel");
 const closeRulesBtn = document.getElementById("close-rules");
 const openRulesBtn = document.getElementById("rules-button");
+const game = document.getElementById("game");
+
+const ctx = game.getContext("2d");
+
+ctx.beginPath();
+ctx.arc(400, 300, 10, 0, 2 * Math.PI);
+ctx.fillStyle = "#fff";
+ctx.fill();
+ctx.stroke();
 
 openRulesBtn.addEventListener("click", () => {
   rulesPanel.classList.toggle("rules--open");

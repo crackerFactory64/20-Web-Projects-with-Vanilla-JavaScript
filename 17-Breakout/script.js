e7ee364ebbd7d362f17a81228376fe36d756d704
crackerFactory64 <lee.mander@gmail.com>
2021-11-09 16:26:18 +0000

    draw game board

--- 17-Breakout/script.js
+++ 17-Breakout/script.js
@@ -1,15 +1,52 @@
 const rulesPanel = document.getElementById("rules-panel");
 const closeRulesBtn = document.getElementById("close-rules");
 const openRulesBtn = document.getElementById("rules-button");
-const game = document.getElementById("game");
+const canvas = document.getElementById("game");
 
 const ctx = game.getContext("2d");
 
-ctx.beginPath();
-ctx.arc(400, 300, 10, 0, 2 * Math.PI);
-ctx.fillStyle = "#fff";
-ctx.fill();
-ctx.stroke();
+const ball = {
+  x: canvas.width / 2,
+  y: canvas.height / 2,
+  size: 10,
+};
+
+const paddle = {
+  x: canvas.width / 2 - 50,
+  y: canvas.height - 20,
+  w: 100,
+  h: 10,
+};
+
+draw();
+
+function draw() {
+  drawBall();
+  drawPaddle();
+  drawScore();
+}
+
+function drawScore() {
+  ctx.font = "50px Helvetica";
+  ctx.fillStyle = "#fff";
+  ctx.fillText("000", 675, 75);
+}
+
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddle.x, paddle.y, paddle.w, paddle.h);
+  ctx.fillStyle = "#fff";
+  ctx.fill();
+  ctx.closePath();
+}
+
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(ball.x, ball.y, ball.size, 0, 2 * Math.PI);
+  ctx.fillStyle = "#fff";
+  ctx.fill();
+  ctx.closePath();
+}
 
 openRulesBtn.addEventListener("click", () => {
   rulesPanel.classList.toggle("rules--open");

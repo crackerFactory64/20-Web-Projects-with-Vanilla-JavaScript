0122f2a6136f842075f0e5fa57cf22fc5b8bc70a
crackerFactory64 <lee.mander@gmail.com>
2021-11-10 14:54:31 +0000

    paddle movement

--- 17-Breakout/script.js
+++ 17-Breakout/script.js
@@ -16,6 +16,8 @@ const paddle = {
   y: canvas.height - 20,
   w: 100,
   h: 10,
+  dx: 0,
+  speed: 10,
 };
 
 const brick = {
@@ -27,20 +29,36 @@ const brick = {
 
 const colors = ["#363AD1", "#429D40", "#CCA407", "#D06537"];
 
+update();
+
+function update() {
+  movePaddle();
+  draw();
+  requestAnimationFrame(update);
+}
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBall();
+  drawPaddle();
+  drawScore();
+  drawBricks();
+}
+
 function drawBricks() {
-  const columns = 9;
+  const columns = 8;
   const rows = 4;
 
   for (let i = 1; i <= rows; i++) {
     let y = 30 * i;
     ctx.beginPath();
-    ctx.rect(0, y, brick.w, brick.h);
+    ctx.rect(50, y, brick.w, brick.h);
     ctx.fillStyle = colors[i - 1];
     ctx.fill();
     ctx.closePath();
     for (let j = 1; j < columns; j++) {
       ctx.beginPath();
-      ctx.rect(brick.x * j, y, brick.w, brick.h);
+      ctx.rect(brick.x * j + 50, y, brick.w, brick.h);
       ctx.fillStyle = colors[i + -1];
       ctx.fill();
       ctx.closePath();
@@ -48,13 +66,32 @@ function drawBricks() {
   }
 }
 
-draw();
+function movePaddle() {
+  paddle.x += paddle.dx;
 
-function draw() {
-  drawBall();
-  drawPaddle();
-  drawScore();
-  drawBricks();
+  if (paddle.x + paddle.w > canvas.width) {
+    paddle.x = canvas.width - paddle.w;
+  }
+
+  if (paddle.x < 0) {
+    paddle.x = 0;
+  }
+}
+
+function keyDown(e) {
+  if (e.key == "ArrowRight") {
+    paddle.dx = paddle.speed;
+    console.log(paddle.x);
+  }
+  if (e.key == "ArrowLeft") {
+    paddle.dx = -paddle.speed;
+  }
+}
+
+function keyUp(e) {
+  if (e.key === "ArrowRight" || e.key === "ArrowLeft") {
+    paddle.dx = 0;
+  }
 }
 
 function drawScore() {
@@ -86,3 +123,6 @@ openRulesBtn.addEventListener("click", () => {
 closeRulesBtn.addEventListener("click", () => {
   rulesPanel.classList.toggle("rules--open");
 });
+
+document.addEventListener("keydown", keyDown);
+document.addEventListener("keyup", keyUp);

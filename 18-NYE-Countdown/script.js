0f8e7127065239a26812f285924beda2401fac20
crackerFactory64 <lee.mander@gmail.com>
2021-11-19 13:48:25 +0000

    functionality

--- 18-NYE-Countdown/script.js
+++ 18-NYE-Countdown/script.js
@@ -9,4 +9,45 @@ const nextYearEL = document.getElementById("next-year");
 let currentTime = new Date();
 const nyd = new Date(`January 1, ${currentTime.getFullYear() + 1}`);
 
+updateTime();
+
+setInterval(() => {
+  updateTime();
+}, 1000);
+
+function updateTime() {
+  currentTime = new Date();
+  daysEl.innerHTML = getDays();
+  hoursEL.innerHTML = getHours();
+  minsEl.innerHTML = getMins();
+  secsEl.innerHTML = getSecs();
+}
+
+function getDays() {
+  return Math.floor((nyd - currentTime) / 86400000);
+}
+
+function getHours() {
+  return currentTime.getMinutes() == 0
+    ? 24 - currentTime.getHours()
+    : 23 - currentTime.getHours();
+}
+
+function getMins() {
+  return 59 - currentTime.getMinutes();
+}
+
+function getSecs() {
+  return 60 - currentTime.getSeconds();
+}
+
+loading();
+
+function loading() {
+  setTimeout(() => {
+    spinner.classList.add("app__spinner--hide");
+    count.classList.add("app__count--show");
+  }, 1500);
+}
+
 nextYearEL.innerHTML = currentTime.getFullYear() + 1;
